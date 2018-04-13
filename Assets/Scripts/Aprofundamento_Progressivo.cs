28f870357465fcb3bfd222faea0eed703917c80f
Janjos <joaoanjos_96@hotmail.com>
2018-04-13 19:07:52 +0100

    Aprofundamento Progressivo comentado

--- Assets/Scripts/Aprofundamento_Progressivo.cs
+++ Assets/Scripts/Aprofundamento_Progressivo.cs
@@ -5,52 +5,53 @@ using UnityEngine;
 public class Aprofundamento_Progressivo : SearchAlgorithm {
 
 	private Stack<SearchState> openStack;
-	public int nivel;//guarda o depth do current state
-	public int maxdepth;
+	public int nivel; //guarda o depth do current state
+    public int maxdepth; //guarda a depth máxima no qual o algoritmo já analizou;
 
 	//1 Começa a fazer a pilha com o estado inicial
-	//2 Entra no ciclo e caso a pilha nao esteja vazia retira o elemento a cabeca da pilha
-	//Analisa e verifica se e solucao
-	//Se for solucao devolve o elemento 
-	//Se nao for solucao determina os sucessores e adiciona os a cabeca da pilha
-	//a diferenca entre profundidade e profundidade limitada e que no 2.3.2
-	// so expandimos se o node for de um nivel inferior ao maximo
+    //2 Entra no ciclo e caso a pilha não esteja vazia retira o elemento à cabeça da pilha (LIFO)
+	//Analisa e verifica se é solução
+	//Se for solucção devolve o elemento 
+	//Se nao for solucao determina os sucessores e adiciona-os à cabeça da pilha
+	//A diferença entre profundidade e profundidade limitada e que no 2.3.2
+	//Só expandimos se o node for de um nível inferior ao maximo
+    //A função recomeça com a posição do "zombie" para cada elemento a encontrar
 
-	protected override void Begin () {							//1 Cria a Pilha
-		startNode = GridMap.instance.NodeFromWorldPoint (startPos);
-		targetNode = GridMap.instance.NodeFromWorldPoint (targetPos);
+	protected override void Begin () {							//1 Inicia a função
+		startNode = GridMap.instance.NodeFromWorldPoint (startPos); //Verifica o nó inicial e a respectiva posição
+        targetNode = GridMap.instance.NodeFromWorldPoint (targetPos); //Verifica o nó destino e a respectiva posição
 
-		//cria a pilha e insere o node inicial
-		SearchState start = new SearchState (startNode, 0);
+		//Cria a pilha e insere o node inicial
+		SearchState start = new SearchState (startNode, 0); //nó inicial
 		openStack = new Stack<SearchState> ();
 		openStack.Push(start);
-		maxdepth = 0;
+		maxdepth = 0; //inicia a maxdepth a 0 pois ainda não verificou nada
 
 	}
 	//está sempre a repetir - sempre a ser chamado
-	protected override void Step () {							//2 Repete
+	protected override void Step () {						//2 Repete
 		//se a pilha não esta vazia
 		if (openStack.Count > 0)
 		{
 			//tira o node
-			SearchState currentState = openStack.Pop();		//2.2 Retira o elemento a cabeca da pilha
+            SearchState currentState = openStack.Pop();		//2.2 Retira o elemento à cabeça da pilha (LIFO)
 			VisitNode (currentState);
-			//verifica se o node e solucao
-			if (currentState.node == targetNode) {//2.3
-				//se e solucao termina
-				solution = currentState;						//2.3.1
+			//verifica se o node é soluçao
+			if (currentState.node == targetNode) { //2.3
+				//se é solucao termina
+				solution = currentState;					//2.3.1
 				finished = true;
 				running = false;
 				foundPath = true;
-			} else {//2.3.2 Insere elementos a cabeca da pilha 
+			} else {                                        //2.3.2 Insere elementos à cabeca da pilha 
 				//efeito de procura em profundidade
-				//se nao e solucao vai buscar os sucessores do no
-				nivel= currentState.depth;
-				if (nivel < maxdepth) {//se o depth do no for menor que o maximo expande
-					foreach (Node suc in GetNodeSucessors(currentState.node)) {
-						//cria um novo estado e um novo no
+				//se não é solução vai buscar os sucessores do nó
+				nivel = currentState.depth;
+				if (nivel < maxdepth) {//se o depth do nó for menor que o máximo expande
+					foreach (Node suc in GetNodeSucessors(currentState.node)) { //Devolve os nós sucessores pela ordem N-E-S-W
+						//cria um novo estado e um novo nó
 						//volta acima
-						SearchState new_node = new SearchState (suc, suc.gCost + currentState.g, currentState);//define o no para onde vais a seguir
+						SearchState new_node = new SearchState (suc, suc.gCost + currentState.g, currentState);//define o nó para onde vais a seguir
 						openStack.Push (new_node);
 					}
 				} 
@@ -60,12 +61,15 @@ public class Aprofundamento_Progressivo : SearchAlgorithm {
 				}
 			}
 	}
-		else //se a pilha estiver vazia termina
-		{	//2.1 Testa se uma pilha nao tem elementos
+		else //Se a pilha estiver vazia termina
+            
+		{	//2.1 Testa se a pilha não tem elementos
 			finished = false;
 			running = true ;
-			foundPath = false;
-			maxdepth++;//aumenta o maximo
+
+			//foundPath = false;
+
+			maxdepth++;//aumenta o máximo
 			SearchState start = new SearchState (startNode, 0);
 			openStack.Push(start);
 
@@ -74,8 +78,5 @@ public class Aprofundamento_Progressivo : SearchAlgorithm {
 
 	}
 }
-
-/*somehow temos de ir buscar o nivel do no e comparar com o nivel maximo
- * se for menor expande e se for maior nao*/
 
 
