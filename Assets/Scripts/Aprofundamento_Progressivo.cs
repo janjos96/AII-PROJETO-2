b90be2b4a0fd576883eadae5f93ff44cbe78978b
Janjos <joaoanjos_96@hotmail.com>
2018-04-13 19:36:33 +0100

    Largura Comentado

--- Assets/Scripts/Aprofundamento_Progressivo.cs
+++ Assets/Scripts/Aprofundamento_Progressivo.cs
@@ -11,10 +11,10 @@ public class Aprofundamento_Progressivo : SearchAlgorithm {
 	//1 Começa a fazer a pilha com o estado inicial
     //2 Entra no ciclo e caso a pilha não esteja vazia retira o elemento à cabeça da pilha (LIFO)
 	//Analisa e verifica se é solução
-	//Se for solucção devolve o elemento 
-	//Se nao for solucao determina os sucessores e adiciona-os à cabeça da pilha
-	//A diferença entre profundidade e profundidade limitada e que no 2.3.2
-	//Só expandimos se o node for de um nível inferior ao maximo
+	//Se for solução devolve o elemento 
+	//Se nao for solução determina os sucessores e adiciona-os à cabeça da pilha
+	//A diferença entre profundidade e profundidade limitada é que no 2.3.2
+	//só expandimos se o node for de um nível inferior ao maximo
     //A função recomeça com a posição do "zombie" para cada elemento a encontrar
 
 	protected override void Begin () {							//1 Inicia a função
@@ -33,10 +33,10 @@ public class Aprofundamento_Progressivo : SearchAlgorithm {
 		//se a pilha não esta vazia
 		if (openStack.Count > 0)
 		{
-			//tira o node
+			//tira o node na cabeça
             SearchState currentState = openStack.Pop();		//2.2 Retira o elemento à cabeça da pilha (LIFO)
 			VisitNode (currentState);
-			//verifica se o node é soluçao
+			//verifica se o node é solução
 			if (currentState.node == targetNode) { //2.3
 				//se é solucao termina
 				solution = currentState;					//2.3.1
@@ -51,8 +51,8 @@ public class Aprofundamento_Progressivo : SearchAlgorithm {
 					foreach (Node suc in GetNodeSucessors(currentState.node)) { //Devolve os nós sucessores pela ordem N-E-S-W
 						//cria um novo estado e um novo nó
 						//volta acima
-						SearchState new_node = new SearchState (suc, suc.gCost + currentState.g, currentState);//define o nó para onde vais a seguir
-						openStack.Push (new_node);
+						SearchState new_node = new SearchState (suc, suc.gCost + currentState.g, currentState); //define o nó que vai adicionar
+						openStack.Push (new_node); //adiciona o nó
 					}
 				} 
 				// for energy
@@ -70,7 +70,7 @@ public class Aprofundamento_Progressivo : SearchAlgorithm {
 			//foundPath = false;
 
 			maxdepth++;//aumenta o máximo
-			SearchState start = new SearchState (startNode, 0);
+			SearchState start = new SearchState (startNode, 0); //Recomeça processo com maxdepth+1
 			openStack.Push(start);
 
 
--- Assets/Scripts/BreadthFirstSearch.cs
+++ Assets/Scripts/BreadthFirstSearch.cs
@@ -6,32 +6,51 @@ public class BreadthFirstSearch : SearchAlgorithm {
 
 	private Queue<SearchState> openQueue;
 
+    //1 Começa a fazer a fila com o estado inicial
+    //2 Entra no ciclo e caso a fila não esteja vazia retira o elemento no fim da fila (FIFO)
+    //Analisa e verifica se é solução
+    //Se for solução devolve o elemento 
+    //Se nao for solução determina os sucessores e adiciona-os ao fim da fila
+    //A diferença entre largura e profundidade limitada é que no 2.3.2
+    //adicionamos os nós sucessores no fim da fila e por ser FIFO o primeiro a entrar vai ser o primeiro a sair
+    //e por isso percorre cada nível de cada vez
+    //A função recomeça com a posição do "zombie" para cada elemento a encontrar
 
-	protected override void Begin () {
-		startNode = GridMap.instance.NodeFromWorldPoint (startPos);
-		targetNode = GridMap.instance.NodeFromWorldPoint (targetPos);
 
-		SearchState start = new SearchState (startNode, 0);
+    protected override void Begin () {                          //1 Inicia a função
+        startNode = GridMap.instance.NodeFromWorldPoint (startPos);  //Verifica o nó inicial e a respectiva posição
+        targetNode = GridMap.instance.NodeFromWorldPoint (targetPos); //Verifica o nó destino e a respectiva posição
+
+        //Cria a fila e insere o node inicial
+        SearchState start = new SearchState (startNode, 0); //nó inicial
 		openQueue = new Queue<SearchState> ();
 		openQueue.Enqueue(start);
 		
 	}
 	
-	protected override void Step () {
-		
+    //está sempre a repetir - sempre a ser chamado
+    protected override void Step () {                       //2 Repete
+        //se a fila não esta vazia
 		if (openQueue.Count > 0)
 		{
-			SearchState currentState = openQueue.Dequeue();
+            //tira o node no fim
+            SearchState currentState = openQueue.Dequeue();         //2.2 Retira o elemento no fim da fila (FIFO)
 			VisitNode (currentState);
-			if (currentState.node == targetNode) {
-				solution = currentState;
+            //verifica se o node é solução
+            if (currentState.node == targetNode) { //2.3
+                //se é solucao termina
+                solution = currentState;                    //2.3.1
 				finished = true;
 				running = false;
 				foundPath = true;
-			} else {
-				foreach (Node suc in GetNodeSucessors(currentState.node)) {
-					SearchState new_node = new SearchState(suc, suc.gCost + currentState.g, currentState);
-					openQueue.Enqueue (new_node);
+            } else {                                        //2.3.2 Insere elementos ao fim da fila 
+                //efeito de procura em largura
+                //se não é solução vai buscar os sucessores do nó
+                foreach (Node suc in GetNodeSucessors(currentState.node)) { //Devolve os nós sucessores pela ordem N-E-S-W
+                    //cria um novo estado e um novo nó
+                    //volta acima
+                    SearchState new_node = new SearchState(suc, suc.gCost + currentState.g, currentState); //define o nó que vai adicionar
+                    openQueue.Enqueue (new_node); //adiciona o nó
 				}
 				// for energy
 				if ((ulong) openQueue.Count > maxListSize) {
@@ -39,11 +58,11 @@ public class BreadthFirstSearch : SearchAlgorithm {
 				}
 			}
 		}
-		else
-		{
+        else //Se a pilha estiver vazia termina
+        {   //2.1 Testa se a pilha não tem elementos
 			finished = true;
 			running = false;
-			foundPath = true;
+			//foundPath = true;
 		}
 
 	}
